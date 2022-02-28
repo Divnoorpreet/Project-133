0dde510ba6ff256a502585ceb327a862aeebe50d
Divnoorpreet <77381888+Divnoorpreet@users.noreply.github.com>
2022-02-28 13:13:17 +0530

    Add files via upload

---
+++ toy2.js
@@ -0,0 +1,48 @@
+img = "";
+status = "";
+objects = [];
+
+function navi(){
+    window.location="index.html";
+}
+
+function preload(){
+    img = loadImage("toy2.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500, 300);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResults);
+}
+
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 600, 480)
+    if(status != ""){
+        for(i = 0;i < objects.length; i++){
+            fill("#FF0000");
+            percent =floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x , objects[i].y);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
